67eacb3b92cfbcf77e0d9276f931d5bf389c9887
sai charan <saicharan0662@gmail.com>
2022-02-25 16:24:35 +0530

    navbar color changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Navbar = ({ path = "/login", text = "Login", isLoading = false }) => {
     return (
         <div className='sticky top-0 left-0' style={{ zIndex: '2' }}>
-            <div className=' w-full flex justify-between align-center' style={{ minHeight: '55px', backgroundColor: "#1059a2" }}>
+            <div className=' w-full flex justify-between align-center bg-deepBlue' style={{ minHeight: '55px' }}>
                 <div className='text-white font-normal my-4 mx-4'>
                     E-Scheduler
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       colors: {
         'light-gray': 'rgba(0, 0, 0, 0.6)',
       },
+      backgroundColor: {
+        'deepBlue': 'rgba(16, 89, 162, 0.78)',
+      }
     },
   },
   plugins: [],
